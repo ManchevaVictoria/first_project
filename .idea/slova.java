e0949491322fcc1f0e76a515a702ab10e4b77cae
ManchevaVictoria <37897901+ManchevaVictoria@users.noreply.github.com>
2020-03-26 13:26:17 +0300

    Update slova.java

--- .idea/slova.java
+++ .idea/slova.java
@@ -1,8 +1,10 @@
-
+import java.util.Scanner;
 
 package first_project;
 public class slova {
-    public static void main() {
-
+public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(s);
     }
 }
